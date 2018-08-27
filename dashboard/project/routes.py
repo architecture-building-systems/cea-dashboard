bb78ea0882abf087a83a7f06cb5a21ca98c8eb86
Daren Thomas <thomas@arch.ethz.ch>
2018-08-27 10:56:55 +0200

    project page now supports the controls

--- dashboard/project/routes.py
+++ dashboard/project/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, current_app, request, redirect, jsonify, url_for
+import os
 import cea.inputlocator
 
 blueprint = Blueprint(
@@ -33,17 +34,87 @@ def route_show():
 def route_save():
     """Save the new project data to the configuration file"""
     cea_config = current_app.cea_config
+    print(request.form)
     cea_config.scenario = request.form['scenario']
     cea_config.region = request.form['region']
-    cea_config.weather = request.form['weather-path']
+    cea_config.weather = request.form['weather']
     cea_config.multiprocessing = 'multiprocessing' in request.form
     cea_config.save()
     return redirect(url_for('project_blueprint.route_show'))
 
 
-@blueprint.route('/weather/<weather_name>')
-def route_weather_path(weather_name):
-    cea_config = current_app.cea_config
-    locator = cea.inputlocator.InputLocator(cea_config.scenario)
-    weather_path = locator.get_weather(weather_name)
-    return jsonify(weather_path)
+# FIXME: this should be refactored, as it is originally from tools/routes.py
+@blueprint.route('/open-folder-dialog/<fqname>')
+def route_open_folder_dialog(fqname):
+    """Return html of folder structure for that parameter"""
+
+    # these arguments are only set when called with the `navigate_to` function on an already open
+    # folder dialog
+    current_folder = request.args.get('current_folder')
+    folder = request.args.get('folder')
+
+    config = current_app.cea_config
+    section, parameter_name = fqname.split(':')
+    parameter = config.sections[section].parameters[parameter_name]
+
+    if not current_folder:
+        # first time calling, use current value of parameter for current folder
+        current_folder = os.path.abspath(parameter.get())
+        folder = None
+    else:
+        current_folder = os.path.abspath(os.path.join(current_folder, folder))
+
+    folders = []
+    for entry in os.listdir(current_folder):
+        if os.path.isdir(os.path.join(current_folder, entry)):
+            folders.append(entry)
+
+    current_folder = os.path.normpath(current_folder)
+    breadcrumbs = current_folder.split(os.path.sep)
+
+    return render_template('folder_listing.html', current_folder=current_folder,
+                           folders=folders, title=parameter.help, fqname=fqname,
+                           parameter_name=parameter.name, breadcrumbs=breadcrumbs)
+
+
+# FIXME: this should be refactored, as it is originally from tools/routes.py
+@blueprint.route('/open-file-dialog/<fqname>')
+def route_open_file_dialog(fqname):
+    """Return html of file structure for that parameter"""
+
+    # these arguments are only set when called with the `navigate_to` function on an already open
+    # file dialog
+    current_folder = request.args.get('current_folder')
+    folder = request.args.get('folder')
+
+    config = current_app.cea_config
+    section, parameter_name = fqname.split(':')
+    parameter = config.sections[section].parameters[parameter_name]
+
+    if not current_folder:
+        # first time calling, use current value of parameter for current folder
+        current_folder = os.path.dirname(parameter.get())
+        folder = None
+    else:
+        current_folder = os.path.abspath(os.path.join(current_folder, folder))
+
+    print('route_open_file_dialog: current_folder=%(current_folder)s' % locals())
+
+    folders = []
+    files = []
+    for entry in os.listdir(current_folder):
+        if os.path.isdir(os.path.join(current_folder, entry)):
+            folders.append(entry)
+        else:
+            ext = os.path.splitext(entry)[1]
+            if parameter._extensions and ext and ext[1:] in parameter._extensions:
+                files.append(entry)
+            elif not parameter._extensions:
+                # any file can be added
+                files.append(entry)
+
+    breadcrumbs = os.path.normpath(current_folder).split(os.path.sep)
+
+    return render_template('file_listing.html', current_folder=current_folder,
+                           folders=folders, files=files, title=parameter.help, fqname=fqname,
+                           parameter_name=parameter.name, breadcrumbs=breadcrumbs)
