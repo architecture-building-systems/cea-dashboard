8c4bfb800b8e9cdead3516784a2d2bd87dd8ed56
Daren Thomas <thomas@arch.ethz.ch>
2018-11-13 15:52:30 +0100

    show errors when loading dashboard plots

--- cea_dashboard/plots/static/js/dashboard-plots.js
+++ cea_dashboard/plots/static/js/dashboard-plots.js
@@ -21,6 +21,10 @@ function load_all_plots() {
 
 function load_plot(category_name, plot_id) {
     $.get('./div/' + category_name + '/' + plot_id, function(data){
-            $('#x_content-' + plot_id).replaceWith(data);
+        $('#x_content-' + plot_id).replaceWith(data);
+    }).fail(function(data) {
+        $('#x_content-' + plot_id).children().replaceWith('ERROR: ' + $(data.responseText).filter('p').text());
+        console.log('error creating plot:');
+        console.log(data);
     });
 }
