72e06bf60ca098f2a8ce2f26f9f420a083949ea5
Daren Thomas <thomas@arch.ethz.ch>
2018-11-15 15:37:26 +0100

    remove unused functions

--- cea_dashboard/plots/routes.py
+++ cea_dashboard/plots/routes.py
@@ -62,6 +62,7 @@ def route_div(dashboard_index, plot_index):
 
 
 def load_plot(dashboard_index, plot_index):
+    """Load a plot from the dashboard_yml"""
     cea_config = current_app.cea_config
     dashboards = cea.plots.read_dashboards(cea_config)
     dashboard_index = dashboards[dashboard_index]
@@ -77,48 +78,3 @@ def route_plot(dashboard_index, plot_index):
         return abort(500, ex)
 
     return render_template('plot.html', dashboard_index=dashboard_index, plot_index=plot_index, plot=plot)
-
-
-def get_plot_parameters(locator, plot):
-    """Return a dictionary of parameters for a plot
-
-    :param InputLocator locator: input locator for the plots
-    :param str plot: name of the plot
-    """
-    parameters = {}
-    plot_data = current_app.plots_data[plot]
-    if 'buildings' in plot_data['parameters']:
-        parameters['buildings'] = (current_app.cea_config.plots.buildings, locator.get_zone_building_names())
-    return parameters
-
-
-def get_plot_fig(locator, plot):
-    plot_data = current_app.plots_data[plot]
-    module_name, class_name = os.path.splitext(plot_data['preprocessor'])
-    class_name = class_name[1:]
-    module = importlib.import_module(module_name)
-
-    config = current_app.cea_config
-    args = {'locator': locator, 'config': config}
-    if 'weather' in plot_data['parameters']:
-        args['weather'] = config.weather
-    if 'buildings' in plot_data['parameters']:
-        valid_buildings = locator.get_zone_building_names()
-        args['buildings'] = [building for building in json.loads(request.args.get('buildings', default='[]'))
-                             if building in valid_buildings]
-    if 'scenarios' in plot_data['parameters']:
-        args['scenarios'] = config.plots.scenarios
-        del args['locator']
-    if 'individual' in plot_data['parameters']:
-        args['individual'] = config.plots.individual
-    if 'generations' in plot_data['parameters']:
-        args['generations'] = config.plots.generations
-    if 'network_type' in plot_data['parameters']:
-        args['network_type'] = config.plots.generations
-    if 'network_names' in plot_data['parameters']:
-        args['network_names'] = config.plots.network_names
-
-    preprocessor = getattr(module, class_name)(**args)
-    plot_function = getattr(preprocessor, plot_data['plot-function'])
-    fig = plot_function(category='dashboard')
-    return fig
